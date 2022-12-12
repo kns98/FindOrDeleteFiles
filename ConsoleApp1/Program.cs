b2e7add0a4b8710cac12e8ac6cb0405660ea7592
Kevin Sheth <kns98@yahoo.com>
2022-12-12 13:02:26 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+class Prgram
+{
+    private static void Main(string[] args)
+    {
+
+        Prgram p = new Prgram();
+        var fileList = p.GetFiles(@"d:\onedrive\pictures");
+
+        foreach (var file in fileList)
+        {
+            Console.WriteLine(file);
+        }
+
+        Console.WriteLine("Press aby key to continue ...");
+        Console.ReadKey();
+    }
+
+    
+
+    private FileInfo[] GetFiles(string path)
+    {
+        var info = new DirectoryInfo(path);
+
+        IEnumerable<FileInfo> fileList1 = info.GetFiles("*.xmp",
+            SearchOption.AllDirectories);
+
+
+        IEnumerable<FileInfo> _fileQuery1 =
+            from file in fileList1
+            where file.Length > 0
+            orderby file.Name
+            select file;
+
+        FileInfo[] fileQuery1 = _fileQuery1.ToArray();
+
+        return fileQuery1;
+
+    }
+
+}
